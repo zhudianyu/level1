8d3119543fef379a9bfa20a5afe6bdba3d1a79f7
zhudianyu <dianyu.zhu@zqgame.com>
2015-04-07 19:20:25 +0800

    add level1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float m_speed = 1;
+	protected Transform m_transform;
+	public Transform m_rocket;
+	float m_rocketRate =0;
+	// Use this for initialization
+	void Start () {
+		m_transform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float movev = 0;
+		float moveh = 0;
+		if (Input.GetKey (KeyCode.UpArrow)) {
+						movev -= m_speed * Time.deltaTime;
+				}
+		if (Input.GetKey (KeyCode.DownArrow))
+		{
+			movev += m_speed*Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			
+			moveh += m_speed*Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			moveh -= m_speed * Time.deltaTime;
+		}
+		m_rocketRate -= Time.deltaTime;
+		if (m_rocketRate <= 0) {
+			m_rocketRate = 0.1f;
+			if (Input.GetKey (KeyCode.Space) || Input.GetMouseButton (0)) {
+				Instantiate(m_rocket,m_transform.position,m_transform.rotation);
+			}
+				}
+
+		this.m_transform.Translate(new Vector3(moveh,0,movev));
+	}
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rocket : MonoBehaviour {
+	public float m_speed = 10;
+	public float m_liveTime = 1;
+	public float m_power = 1;
+	protected Transform m_transform ;
+	// Use this for initialization
+	void Start () {
+		m_transform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_liveTime -= Time.deltaTime;
+		if (m_liveTime <= 0)
+						Destroy (this.gameObject);
+		m_transform.Translate (new Vector3 (0, 0, -m_speed * Time.deltaTime));
+	}
+}
